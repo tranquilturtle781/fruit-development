1f37e2163d5fed82a53b6519bf6168dc8cc81857
tranquilturtle781 <115186235+tranquilturtle781@users.noreply.github.com>
2022-11-29 17:28:04 -0500

    Update FavsList.js

--- src/components/FavsList.js
+++ src/components/FavsList.js
@@ -2,11 +2,11 @@ import React from 'react'
 import "../App.css"
 import Fruit from "./Fruit.js";
 
-export default function FavsList({favs, data, modifyFavs}) {
+export default function FavsList({favs, modifyFavs}) {
     return(
         <div className="favs">
         {
-        data.filter(x => favs.includes(x)).map((item, index) => (
+        favs.map((item, index) => (
             <Fruit item={item} changeFavs={modifyFavs} key={index}></Fruit>
           ))
         }
